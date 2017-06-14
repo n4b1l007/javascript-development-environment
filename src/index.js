2e233f980fceb78b0e22b2d14279ad1fd597a845
Jafrul Sadek <nabil@bssitbd.com>
2017-06-14 18:39:24 +0600

    Source Added

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import './index.css'
+import { getUsers } from './api/userApi';
+
+
+getUsers().then(result => {
+    let userBody = "";
+
+    result.forEach(user => {
+        userBody += `<tr>
+                    <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+                    <td>${user.id}</td>
+                    <td>${user.firstName}</td>
+                    <td>${user.lastName}</td>
+                    <td>${user.email}</td>
+                    </tr>`
+    });
+    global.document.getElementById('users').innerHTML = userBody;
+});
+
+
+
+
+
+
+
+
+
+
+// import numeral from 'numeral';
+
+// const courseValue = numeral(1000).format('$0,0.00');
+// console.log(`I would pay ${courseValue} for this awesome course!`); //eslint-disable-line no-console
